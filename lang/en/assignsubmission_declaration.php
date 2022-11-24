35b25991092174c1e46b21b5a7e55b72e94d212f
Veronica Bermegui <verisb@gmail.com>
2022-11-24 11:48:27 +1100

    Fix typo

--- lang/en/assignsubmission_declaration.php
+++ lang/en/assignsubmission_declaration.php
@@ -28,7 +28,7 @@ $string['allowonlinetextsubmissions'] = 'Enabled';
 $string['default'] = 'Enabled by default';
 $string['default_help'] = 'If set, this submission method will be enabled by default for all new assignments.';
 $string['enabled'] = 'Declaration submissions';
-$string['enabled_help'] = 'If enabled, students are able to declare concent in their submission.';
+$string['enabled_help'] = 'If enabled, students are able to declare consent in their submission.';
 $string['title'] = 'Declaration 1';
 $string['dummy_declaration'] = 'orem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim odio ut mauris ultrices, ac cursus tortor aliquet. Duis auctor ante in lacinia viverra. Curabitur eget accumsan nunc. Nam vel arcu nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean efficitur sapien eget convallis tempus. Ut et erat ac elit auctor tempor id eget libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum id fringilla elit. Mauris cursus lobortis lectus porta venenatis. Nullam cursus tortor ligula, vitae vestibulum leo ultricies at. Vestibulum pulvinar neque dolor, quis dictum mi dignissim eget. Nam pharetra, lacus non cursus consequat, ipsum nulla luctus turpis, non ultricies magna ex sit amet nisl. Vivamus ultrices mi erat, ac facilisis arcu convallis nec. Suspendisse blandit tincidunt felis nec porttitor.
 
